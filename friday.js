86834599421f63c9849d8a45654202ea48d9faa4
Gino Guerra <ginoiguerra@gmail.com>
2023-10-06 14:51:29 -0400

    calculate celsius function

---
+++ friday.js
@@ -0,0 +1,14 @@
+function calcFarenheit(event){
+    event.preventDefault() 
+const farenheit = event.target.enterFarenheit.value
+const celsius = (farenheit - 32) * 5 / 9
+
+const h2 = document.querySelector("h2")
+h2.innerText = farenheit + "°F is " + celsius + "°C" 
+}
+
+const form = document.querySelector("form")
+form.addEventListener('submit',calcFarenheit)
+
+
+ 
